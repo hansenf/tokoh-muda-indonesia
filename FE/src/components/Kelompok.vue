790e2615bbcdf4feb55c33ef9a2e34801bef345b
rizalfmaulana <rizalfmaulana@gmail.com>
2021-11-16 08:54:21 +0700

    base project FE

---
+++ FE/src/components/Kelompok.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Tokoh muda Indonesia</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "kelompok",
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
