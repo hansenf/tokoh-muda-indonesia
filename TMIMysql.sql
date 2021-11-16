48f1fb77d901d9a1fced2d1b3c93b32753acc9d3
miraclemaker17 <hansenferiandy@gmail.com>
2021-11-16 18:27:48 +0700

    add BE

---
+++ TMIMysql.sql
@@ -0,0 +1,85 @@
+CREATE TABLE `mahasiswa` (
+  `id` integer PRIMARY KEY,
+  `username` varchar(255),
+  `password` varchar(255),
+  `nomorHP` integer,
+  `email` integer,
+  `url_foto` varchar(255),
+  `nama_lengkap` varchar(255),
+  `tanggal_lahir` varchar(255),
+  `jenis_kelamin` varchar(255),
+  `asal_kampus` varchar(255),
+  `nim` varchar(255),
+  `jurusan` varchar(255),
+  `tahun_masuk` varchar(255),
+  `kota_kabupaten` varchar(255),
+  `id_tantangan` integer,
+  `id_silabus` integer,
+  `id_event` integer
+);
+
+CREATE TABLE `tantangan` (
+  `id` integer PRIMARY KEY,
+  `judul` varchar(255),
+  `tema` varchar(255),
+  `latar` varchar(255),
+  `url_video` varchar(255),
+  `task` varchar(255),
+  `ujian` varchar(255),
+  `skor` varchar(255),
+  `id_admin` integer
+);
+
+CREATE TABLE `silabus` (
+  `id` integer PRIMARY KEY,
+  `judul` varchar(255),
+  `definisi` varchar(255),
+  `fungsi` varchar(255),
+  `deskripsi` varchar(255),
+  `url_video` varchar(255),
+  `task` varchar(255),
+  `ujian` varchar(255),
+  `skor` varchar(255),
+  `id_admin` integer
+);
+
+CREATE TABLE `admin` (
+  `id` integer PRIMARY KEY,
+  `username` varchar(255),
+  `password` varchar(255),
+  `role` varchar(255)
+);
+
+CREATE TABLE `event` (
+  `id` integer PRIMARY KEY,
+  `judul` varchar(255),
+  `deskripsi` varchar(255),
+  `kriteria` varchar(255),
+  `tanggal` varchar(255),
+  `id_admin` integer
+);
+
+CREATE TABLE `leaderboard` (
+  `ranking` integer,
+  `id_mahasiswa` integer,
+  `id_tantangan` integer,
+  `id_silabus` integer
+);
+
+ALTER TABLE `mahasiswa` ADD FOREIGN KEY (`id_tantangan`) REFERENCES `tantangan` (`id`);
+
+ALTER TABLE `mahasiswa` ADD FOREIGN KEY (`id_silabus`) REFERENCES `silabus` (`id`);
+
+ALTER TABLE `mahasiswa` ADD FOREIGN KEY (`id_event`) REFERENCES `event` (`id`);
+
+ALTER TABLE `tantangan` ADD FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id`);
+
+ALTER TABLE `silabus` ADD FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id`);
+
+ALTER TABLE `event` ADD FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id`);
+
+ALTER TABLE `leaderboard` ADD FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id`);
+
+ALTER TABLE `leaderboard` ADD FOREIGN KEY (`id_tantangan`) REFERENCES `tantangan` (`id`);
+
+ALTER TABLE `leaderboard` ADD FOREIGN KEY (`id_silabus`) REFERENCES `silabus` (`id`);
