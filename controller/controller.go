0c6140f01e9401d816b9dcb33d7ac39de6585e65
raflimr <raflimuhamadridhwan@gmail.com>
2021-11-18 20:56:52 +0700

    JWT Auth- NoHp dengan Password

---
+++ controller/controller.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"encoding/json"
+	"tmi/models"
+	"tmi/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+//login contorller interface
+type LoginController interface {
+	Login(ctx *gin.Context) string
+}
+
+type loginController struct {
+	loginService service.LoginService
+	jWtService   service.JWTService
+}
+
+func LoginHandler(loginService service.LoginService,
+	jWtService service.JWTService) LoginController {
+	return &loginController{
+		loginService: loginService,
+		jWtService:   jWtService,
+	}
+}
+
+func (controller *loginController) Login(ctx *gin.Context) string {
+	var credential models.LoginCredentials
+	err := ctx.ShouldBind(&credential)
+	if err != nil {
+		return "no data found"
+	}
+	isUserAuthenticated := controller.loginService.LoginUser(json.Number(credential.Nohp), credential.Password)
+	if isUserAuthenticated {
+		return controller.jWtService.GenerateToken(json.Number(credential.Nohp), true)
+
+	}
+	return ""
+}
