f59c970047841d7a6f620561cfb2ce1a238cef74
rizalfmaulana <rizalfmaulana@gmail.com>
2021-11-23 10:53:51 +0700

    install vuetify and router

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-    <Kelompok />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-  import Kelompok from "./components/Kelompok.vue";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  export default {
-    name: "App",
-    components: {
-      Kelompok,
-    },
-  };
-</script>
+#nav {
+  padding: 30px;
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+  a {
+    font-weight: bold;
     color: #2c3e50;
-    margin-top: 60px;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
+}
 </style>
